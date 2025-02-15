27bd6fab1d540318cbd9801df086175af75a1aa8
Andreas Mautsch <amautsch>
2025-02-15 12:10:24 +0100

    techradar

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -27,6 +27,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { MyGroupsSidebarItem } from '@backstage/plugin-org';
 import GroupIcon from '@material-ui/icons/People';
+import RadarIcon from '@material-ui/icons/Explore';
 
 const useSidebarLogoStyles = makeStyles({
   root: {
@@ -74,6 +75,8 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         />
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
+        <SidebarItem icon={RadarIcon} to="/tech-radar" text="Tech Radar"/>
+
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
         {/* End global nav */}
         <SidebarDivider />
