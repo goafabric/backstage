facde7075043368fa54c9dcc2510c9d30fe33dd6
Andreas Mautsch <amautsch>
2025-02-16 09:42:08 +0100

    techradar

---
+++ build.sh
@@ -0,0 +1,9 @@
+IMAGE_NAME=goafabric/backstage:1.0.0-SNAPSHOT
+
+yarn install --immutable && yarn tsc && yarn build:backend
+
+docker buildx create --name mybuilder --use && docker buildx build --platform linux/amd64,linux/arm64 -t $IMAGE_NAME -f .packages/backend/Dockerfile --push . ; docker buildx stop mybuilder && docker buildx rm mybuilder
+
+#docker image build . -f packages/backend/Dockerfile --tag ${IMAGE_NAME} && docker push ${IMAGE_NAME}
+
+docker run --rm -it -p 7007:7007 ${IMAGE_NAME}
