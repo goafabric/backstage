b22eb334e460297ad45e95be173c0c6cdfc4ab05
Andreas Mautsch <amautsch>
2025-04-22 08:48:29 +0200

    changes

--- doc/install/create.sh
+++ doc/install/create.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 # create
-time npx @backstage/create-app@latest
-yarn install
+#time npx @backstage/create-app@latest
 
 # cp app-config.yaml + catalog
+
+# adr backend
+yarn --cwd packages/backend add @backstage-community/plugin-adr-backend
+
+sed -i '' "/backend\.start();/i\\
+backend.add(import('@backstage-community/plugin-adr-backend'));
+" packages/backend/src/index.ts
+
+# adr frontend
+yarn --cwd packages/app add @backstage-community/plugin-adr
+
+sed -i '' $'/const techdocsContent/i\\
+import { EntityAdrContent, isAdrAvailable } from '\''@backstage-community/plugin-adr'\'';
+' packages/app/src/components/catalog/EntityPage.tsx
+
+perl -i -pe 's{(</EntityLayout>)}{<EntityLayout.Route if={isAdrAvailable} path="/adrs" title="ADRs">\n  <EntityAdrContent />\n</EntityLayout.Route>\n$1}g' packages/app/src/components/catalog/EntityPage.tsx
+
