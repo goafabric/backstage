d9a2f9b798ba970f5167b2d729be420fd02324ec
Andreas Mautsch <amautsch>
2025-05-15 09:04:11 +0200

    changes

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -77,7 +77,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
 
-        <SidebarItem icon={RadarIcon} to="/tech-radar" text="Tech Radar"/>
+        <SidebarItem icon={RadarIcon} to="tech-radar" text="Tech Radar"/>
 
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
         {/* End global nav */}
