b680d1b31cba6fc58b7df55f75859711d5ccd31d
Andreas Mautsch <amautsch>
2025-05-15 09:13:22 +0200

    changes

--- packages/app/src/components/custom/TechRadar.tsx
+++ packages/app/src/components/custom/TechRadar.tsx
@@ -8,7 +8,7 @@ export const TechRadar = () => (
 
     <Content>
       <iframe
-        src="/custom/techradar/index.html"
+        src="custom/techradar/index.html"
         width="100%"
         height="800px"
         style={{ border: 'none' }}
